186c1bbee1628309b6ec4b5b4031dc64ed7932a1
Louis Shi <louis.shi@uconn.edu>
2018-03-01 05:24:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function happyHolidays (string){
 function happyHolidaysTo (name){
   return `Happy holidays, ${name}!`;
 }
+console.log(happyHolidayTo(Louis));
+
 function happyHolidayTo (holiday, name){
   return 'Happy Independence Day, you!'
 }
