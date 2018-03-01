e538b8b123439d2c7dd4b947341511dd79f586cf
Louis Shi <louis.shi@uconn.edu>
2018-03-01 05:27:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo (name){
   return `Happy holidays, ${name}!`;
 }
 function happyHolidayTo (holiday, name){
-  return 'Happy Independence Day, you!'
+  return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown (days, holiday){
   return 'It\'s 20 days until Mother\'s Day!'
